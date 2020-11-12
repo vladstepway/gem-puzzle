00033fc05d09f10ba23e040bdb467c80a827709b
ustsepavy <vladstepovoyvios@gmail.com>
2020-11-12 22:49:10 +0300

    feat: added menu

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import PuzzleBoard from './js/components/puzzle-board';
+import Menu from './js/components/menu';
 import * as constants from './js/utils/constants';
 
 const settings = { size: constants.DEFAULT_PUZZLE_SIZE };
 
-new PuzzleBoard(settings).init().generatePuzzles();
+new Menu(settings).init();
+// new PuzzleBoard(settings).init().generatePuzzles();
---
+++ src/js/components/menu.js
@@ -0,0 +1,55 @@
+import PuzzleBoard from './puzzle-board';
+import create from '../utils/create';
+
+export default class Menu {
+  constructor(settings) {
+    this.settings = settings;
+  }
+
+  init() {
+    this.overlay = create('div', 'menu_overlay', null, null);
+    this.menu = create('ul', 'menu', null, this.overlay);
+    this.newGame = create('li', 'menu__item', 'New game', this.menu);
+    this.continue = create('li', 'menu__item', 'Resume game', this.menu);
+    this.save = create('li', 'menu__item', 'Save game', this.menu);
+    this.reload = create('li', 'menu__item', 'Reload game', this.menu);
+    this.overlay.classList.add('menu_overlay-active');
+    document.body.prepend(this.overlay);
+
+    this.newGame.addEventListener('click', this.startGame);
+    this.continue.addEventListener('click', this.resumeGame);
+    this.reload.addEventListener('click', this.reloadGame);
+    this.save.addEventListener('click', this.saveGame);
+  }
+
+  startGame = () => {
+    if (this.puzzleBoard) {
+      const main = document.querySelector('main');
+      main.removeChild(main.firstChild);
+    }
+    this.puzzleBoard = new PuzzleBoard(this.settings);
+    this.puzzleBoard.init().generatePuzzles();
+    this.overlay.classList.remove('menu_overlay-active');
+  };
+
+  resumeGame = () => {
+    if (!this.puzzleBoard) {
+      return;
+    }
+    this.overlay.classList.remove('menu_overlay-active');
+  };
+
+  reloadGame = () => {
+    if (!this.puzzleBoard) {
+      return;
+    }
+    this.puzzleBoard.reloadGame();
+    this.overlay.classList.remove('menu_overlay-active');
+  };
+  saveGame = () => {
+    if (!this.puzzleBoard) {
+      return;
+    }
+    this.overlay.classList.remove('menu_overlay-active');
+  };
+}
